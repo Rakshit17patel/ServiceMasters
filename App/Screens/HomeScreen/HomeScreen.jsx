5bc893745f6234f598fdab0bbd03b4bc2ae37451
Rakshit <rakshit17p@gmail.com>
2024-02-25 15:38:33 -0800

    Service master prototype

---
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,30 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from './Header'
+import Slider from './Slider'
+import Categories from './Categories'
+import BusinessList from './BusinessList'
+import { ScrollView } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <>
+      <Header/>
+
+    <ScrollView
+    showsVerticalScrollIndicator={false}>
+      {/* Header  */}
+      <View style={{padding:20}}>
+        {/* Slider  */}
+        <Slider/>
+        {/* Categories  */}
+        <Categories/>
+        {/* Business List  */}
+        <BusinessList/>
+      </View>
+    
+    </ScrollView>
+    </>
+
+  )
+}
---
+++ App/Utils/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  BLACK: "#000000",
+  PRIMARY: "#17A3E0",
+  WHITE: "#ffffff",
+  LIGHT_GRAY: "#EDEDED",
+  PRIMARY_LIGHT: "#E0DBC7",
+  GRAY: "#7f7f7f",
+  LIGHT_GREEN: "#BEF2C3",
+  LIGHT_RED: "#F2BEBE",
+  RED: "#F81212",
+  GREEN: "#04A814",
+};
